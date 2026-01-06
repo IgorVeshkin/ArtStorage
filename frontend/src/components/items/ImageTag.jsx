b0aa7631ee8a5c7425f9a00f182d001a5dab2d31
IgorVeshkin <IgorVeshkin.official@yandex.ru>
2026-01-06 14:58:23 +0300

    Changed the way ImageTag is being rendered by conditional render

--- frontend/src/components/items/ImageTag.jsx
+++ frontend/src/components/items/ImageTag.jsx
@@ -3,6 +3,8 @@ import React, { useRef } from "react";
 // mui-компоненты
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import CloseIcon from '@mui/icons-material/Close';
+import { IconButton } from '@mui/material';
 
 import { Link } from "react-router-dom";
 
@@ -20,23 +22,34 @@ const ImageTag = ({ tag_slug, tag_name, displayOnly=false }) => {
 
     const tagColorRef = useRef(selectTagColor());
 
-    return (
-            <Box sx={{ backgroundColor: tagColorRef.current, borderRadius: "10px", px: 1.5, py: 0.5, }}>
+    const handleDelete = () => {
+        alert("Deleting Tag: " + tag_name);
+    }
 
-                {displayOnly
+    if (displayOnly) {
 
-                    ?
-
+        return <Box sx={{ display: 'flex', alignItems: 'center', backgroundColor: tagColorRef.current, borderRadius: "10px", pl: 1.5, pr: 1.0, py: 0.5, }}>
                     <Box style={{ textDecoration: "none", color: "inherit", }}>
                         <Typography variant="body1"> {tag_name} </Typography>
                     </Box>
 
-                    :
+                    <IconButton
+                        size="small"
+                        sx={{ marginLeft: 0.5, p: 0, }}
+                        onClick={handleDelete}
+                    >
+                        <CloseIcon fontSize="small" />
+                    </IconButton>
+                </Box>
 
-                    <Link to={"/?page=1&tags=" + tag_slug} style={{ textDecoration: "none", color: "inherit", }}>
-                        <Typography variant="body1"> {tag_name} </Typography>
-                    </Link>
-                }
+        }
+
+    return (
+            <Box sx={{ backgroundColor: tagColorRef.current, borderRadius: "10px", px: 1.5, py: 0.5, }}>
+
+                <Link to={"/?page=1&tags=" + tag_slug} style={{ textDecoration: "none", color: "inherit", }}>
+                    <Typography variant="body1"> {tag_name} </Typography>
+                </Link>
 
             </Box>
     );
